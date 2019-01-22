3b5bb9826b9a39fe2b006b05dc35e7ab0e5860c1
Taen Ahammed <taenahammed7@gmail.com>
2019-01-22 11:18:43 +0600

    data type & variables

---
+++ data type & variables/datatypeVariablePlaceholder.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    char name = 'T';
+    int age = 20;
+    float height = 5.5;
+    int weight = 45;
+
+    printf("Hi, my name is: %c \n", name);
+    printf("My age is: %d \n", age);
+    printf("My height is: %f \n", height);
+    printf("My weight is: %d \n", weight);
+
+    return 0;
+}
