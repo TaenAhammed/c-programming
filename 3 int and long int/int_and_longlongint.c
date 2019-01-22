535d9f71318625f8c529cd2189380615d17e4290
Taen Ahammed <taenahammed7@gmail.com>
2019-01-22 11:49:11 +0600

    integer and long integer sizeof INT_MAX

---
+++ 3 int and long int/int_and_longlongint.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int a = 2147483647; // max limit of integer.
+    long long int b = 2147483648;
+
+    printf("%d\n", a);
+    printf("%lld\n", b);
+    return 0;
+}
---
+++ 3 int and long int/sizeof.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main() {
+    printf("The size of int in my computer is: %d\n", sizeof(int));
+    return 0;
+}
