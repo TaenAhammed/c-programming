3e0f88cf230994b5031e5735f178dbd63ec80ff4
Taen Ahammed <taenahammed7@gmail.com>
2019-01-22 13:41:41 +0600

    scanf

---
+++ 6 user input scanf()/scanf().c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int a;
+    char ch;
+    /*
+    printf("Please enter a number: \n");
+    scanf("%d", &a);
+    printf("You entered, %d\n", a);
+    */
+    printf("Please enter a character: \n");
+    scanf("%c", &ch);
+    printf("You entered, %c\n", ch);
+
+return 0;
+}
