c7065eee027584fb35da12d0573274a7077be793
Taen Ahammed <taenahammed7@gmail.com>
2019-01-22 13:30:40 +0600

    character and ascii code

---
+++ 5 character & ASCII code/char & ascii dode.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    char ch = 'M';  // string always be coated in between single coat.
+    printf("%c\n", ch);
+
+    char a = 'A', b = 'B', c = 'C';
+    printf("%d %d %d\n", a, b, c);
+    int x = 65, y = 66, z = 67;
+    printf("%c %c %c", x, y, z);
+return 0;
+}
